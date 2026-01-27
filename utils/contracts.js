3407cb50ac7a7d79c9a602b21386f30252ac0b58
ergonber <ernesto.gonzales.bermudez@gmail.com>
2026-01-27 18:03:08 -0400

    Create contracts.js

---
+++ utils/contracts.js
@@ -0,0 +1,22 @@
+export const CONTRACT_CONFIG = {
+  ADDRESS: "0xTU_CONTRATO_AQUI", // Actualizar después del deployment
+  SONIC_RPC_URL: "https://rpc.testnet.soniclabs.com",
+  CHAIN_ID: 14601,
+  EXPLORER_URL: "https://testnet.soniclabs.com"
+};
+
+export const CONTRACT_ABI = [
+  {
+    "inputs": [
+      {"internalType": "string","name": "_fullName","type": "string"},
+      {"internalType": "string","name": "_courseTitle","type": "string"},
+      {"internalType": "uint256","name": "_date","type": "uint256"},
+      {"internalType": "string","name": "_grade","type": "string"},
+      {"internalType": "string","name": "_cid","type": "string"}
+    ],
+    "name": "createCertificate",
+    "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
