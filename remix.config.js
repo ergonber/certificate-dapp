f5a1f93e1d2d28df01be8ff1252510ae847330ba
ergonber <ernesto.gonzales.bermudez@gmail.com>
2026-01-27 19:05:14 -0400

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverModuleFormat: "cjs",
+  future: {
+    v2_meta: false,
+    v2_errorBoundary: false,
+    v2_normalizeFormMethod: false,
+    v2_headers: false,
+    v2_routeConvention: false
+  }
 };
