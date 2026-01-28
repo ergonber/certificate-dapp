b69ab81586c126e75abd0b03aee29ecedee5582a
ergonber <ernesto.gonzales.bermudez@gmail.com>
2026-01-28 11:30:31 -0400

    Create _index.jsx

---
+++ app/routes/_index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Página de Inicio</h1>
+      <p>Si ves esto, Remix está funcionando</p>
+    </div>
+  );
+}
