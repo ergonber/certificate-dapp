6ce32f4c5b9b95c48336661d5fea21fa56103966
ergonber <ernesto.gonzales.bermudez@gmail.com>
2026-01-28 10:25:31 -0400

    Update contracts.js

--- utils/contracts.js
+++ utils/contracts.js
@@ -1,5 +1,5 @@
 export const CONTRACT_CONFIG = {
-  ADDRESS: "0xAe48Ed8cD53e6e595E857872b1ac338E17F08549", // Actualizar después del deployment
+  ADDRESS: "0xAe48Ed8cD53e6e595E857872b1ac338E17F08549",
   SONIC_RPC_URL: "https://rpc.testnet.soniclabs.com",
   CHAIN_ID: 14601,
   EXPLORER_URL: "https://testnet.soniclabs.com"
@@ -17,6 +17,29 @@ export const CONTRACT_ABI = [
     "name": "createCertificate",
     "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
     "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{"internalType": "uint256","name": "_id","type": "uint256"}],
+    "name": "getCertificate",
+    "outputs": [
+      {"internalType": "uint256","name": "id","type": "uint256"},
+      {"internalType": "string","name": "fullName","type": "string"},
+      {"internalType": "string","name": "courseTitle","type": "string"},
+      {"internalType": "uint256","name": "date","type": "uint256"},
+      {"internalType": "string","name": "grade","type": "string"},
+      {"internalType": "string","name": "cid","type": "string"},
+      {"internalType": "address","name": "issuer","type": "address"},
+      {"internalType": "uint256","name": "createdAt","type": "uint256"}
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "certificateCount",
+    "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+    "stateMutability": "view",
     "type": "function"
   }
 ];
